b53d76465d61f76dabb8906744fb8a7d909a496f
peter szemraj <peterszemraj@gmail.com>
2023-05-09 08:04:10 +0200

    🎨 polish base example
    Signed-off-by: peter szemraj <peterszemraj@gmail.com>

--- examples/inference-example.py
+++ examples/inference-example.py
@@ -6,7 +6,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(mess
 import fire
 import torch
 from datasets import load_dataset
-from transformers import AutoTokenizer, BartForConditionalGeneration
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 
 from unlimiformer import Unlimiformer, UnlimiformerArguments
 
@@ -14,14 +14,25 @@ from unlimiformer import Unlimiformer, UnlimiformerArguments
 def load_and_prepare_models(
     modelname: str, tokenizer_name: str = "facebook/bart-base"
 ) -> tuple:
+    if tokenizer_name is None:
+        tokenizer_name = modelname
+    model = AutoModelForSeq2SeqLM.from_pretrained(modelname)
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_name)
-    model = BartForConditionalGeneration.from_pretrained(modelname)
-    return tokenizer, model
+    return model, tokenizer
 
 
 def process_example(
     tokenizer, example_input: str, max_input_length: int = 1024, device: str = "cpu"
 ) -> tuple:
+    """
+    process_example - process an example input for inference & comparison
+
+    :param AutoTokenizer tokenizer: tokenizer to use
+    :param str example_input: text input to process
+    :param int max_input_length: maximum input length for the truncated example, defaults to 1024
+    :param str device: device to use, defaults to "cpu"
+    :return tuple: tuple of example tokens and truncated example tokens
+    """
     example_tokens = tokenizer(example_input, truncation=False, return_tensors="pt")
     truncated_example_tokens = tokenizer(
         example_input, truncation=True, max_length=max_input_length, return_tensors="pt"
@@ -35,6 +46,7 @@ def process_example(
 def prepare_unlimiformer_kwargs(
     tokenizer, defaults: UnlimiformerArguments = None
 ) -> dict:
+    """prepare_unlimiformer_kwargs - prepare the unlimiformer kwarg (here, use the defaults)"""
     defaults = UnlimiformerArguments() if defaults is None else defaults
     unlimiformer_kwargs = {
         "layer_begin": defaults.layer_begin,
@@ -60,35 +72,38 @@ def generate_output(model, example, tokenizer, max_new_tokens: int = 512) -> str
     return tokenizer.batch_decode(
         model.generate(**example, max_new_tokens=max_new_tokens),
         ignore_special_tokens=True,
+        clean_up_tokenization_spaces=True,
     )[0]
 
 
 def run_inference(
-    modelname: str = "abertsch/unlimiformer-bart-govreport-alternating",
+    model_name: str = "abertsch/unlimiformer-bart-govreport-alternating",
     tokenizer_name: str = "facebook/bart-base",
     dataset_name: str = "urialon/gov_report_validation",
     split_name: str = "validation",
-    source_column: str = "input",
+    input_column: str = "input",
+    example_index: int = 0,
     max_length: int = 1024,
     max_new_tokens: int = 512,
 ) -> None:
     """
-    run_inference -  basic inference example
+    run_inference - Basic example of running inference with Unlimiformer
 
-    :param str modelname: _description_, defaults to "abertsch/unlimiformer-bart-govreport-alternating"
-    :param str tokenizer_name: _description_, defaults to "facebook/bart-base"
-    :param str dataset_name: _description_, defaults to "urialon/gov_report_validation"
-    :param str split_name: _description_, defaults to "validation"
-    :param str source_column: _description_, defaults to "input"
-    :param int max_length: _description_, defaults to 1024
-    :param int max_new_tokens: _description_, defaults to 512
+    :param str model_name: name of the Unlimiformer model to use, defaults to "abertsch/unlimiformer-bart-govreport-alternating"
+    :param str tokenizer_name: model/repo name of the tokenizer to use, defaults to "facebook/bart-base"
+    :param str dataset_name: name of the dataset to use, defaults to "urialon/gov_report_validation"
+    :param str split_name: name of the dataset split to use, defaults to "validation"
+    :param str input_column: name of the column in the dataset to use as input, defaults to "input"
+    :param int example_index: index of the example to use, defaults to 0
+    :param int max_length: maximum length of the input (for the base model), defaults to 1024 (BART-base max)
+    :param int max_new_tokens: maximum number of tokens to generate, defaults to 512
     """
     logger = logging.getLogger(__name__)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     dataset = load_dataset(dataset_name)
 
-    tokenizer, model = load_and_prepare_models(modelname, tokenizer_name)
-    example_input = dataset[split_name][0][source_column]
+    model, tokenizer = load_and_prepare_models(model_name, tokenizer_name)
+    example_input = dataset[split_name][example_index][input_column]
     example, truncated_example = process_example(
         tokenizer, example_input, max_length, device
     )
@@ -99,7 +114,7 @@ def run_inference(
     truncated_out = generate_output(
         model, truncated_example, tokenizer, max_new_tokens=max_new_tokens
     )
-    print(f"Standard BART output:\t{truncated_out}")
+    print(f"Standard BART output:\t{truncated_out}", "\n" * 2)
     # setup model
     defaults = UnlimiformerArguments()
     unlimiformer_kwargs = prepare_unlimiformer_kwargs(
